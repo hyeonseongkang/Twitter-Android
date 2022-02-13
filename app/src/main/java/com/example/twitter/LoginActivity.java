1e9ff513dad5435838166a72391f4a8d83f7da53
hyeonseongkang <gt9697@naver.com>
2022-02-13 14:25:56 +0900

    firebase email login

--- app/src/main/java/com/example/twitter/LoginActivity.java
+++ app/src/main/java/com/example/twitter/LoginActivity.java
@@ -1,14 +1,84 @@
 package com.example.twitter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static String TAG = "LoginAcitivity";
+    
+    private FirebaseAuth mAuth;
+
+    private AppCompatButton signInButton;
+
+    private EditText userEmail, userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signInButton = (AppCompatButton) findViewById(R.id.signInButton);
+        signInButton.setPaintFlags(signInButton.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        userEmail = (EditText) findViewById(R.id.userEmail);
+        userPassword = (EditText) findViewById(R.id.userPassword);
+        
+
+
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                String email = userEmail.getText().toString();
+                String password = userPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Email or Password is Empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "Success");
+                                } else {
+                                    Log.d(TAG, "Fail : " + task.getException());
+                                }
+
+                            }
+                        });
+            }
+        });
+
+
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Toast.makeText(this, "nothing users", Toast.LENGTH_SHORT).show();
+        }
     }
 }
